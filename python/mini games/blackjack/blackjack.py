963bf3428b59cee05b997e0060905ea76f231b81
Frankataccess <frank.mccann.355@accesscreative.ac.uk>
2023-11-01 09:20:57 +0000

    lil bit of blackjack

--- python/mini games/blackjack/blackjack.py
+++ python/mini games/blackjack/blackjack.py
@@ -1,4 +1,30 @@
 import random
 
 def getValue(card):
-    
+    try:
+        return int(card)
+    except:
+        if card == "J" or "Q" or "K":
+            return 10
+        else:
+            return 11
+        
+print("Automatic Blackjack Player\n")
+
+games = 0
+gameOver=False
+
+while not gameOver:
+    deck = ["A","1","2","3","4","5","6","7","8","9","10","J","K"]
+    random.shuffle(deck)
+
+    hand = []
+    score = 0
+
+    while score < 17 and len(deck) ! = 0:
+        card = deck.pop()
+        hand.append(card)
+        score = score + getValue(card)
+
+        if score = 21:
+            pri
