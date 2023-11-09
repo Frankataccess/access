3a2bd7bfa2056d89aa01a3240ccab1bbeb65c277
Frankataccess <frank.mccann.355@accesscreative.ac.uk>
2023-11-09 10:59:47 +0000

    snake 1.0.0.4

--- python/mini games/snake/snake.py
+++ python/mini games/snake/snake.py
@@ -1,10 +1,13 @@
 import pygame, sys, random
 from pygame.math import Vector2
 
+
+
+
 class SNAKE:
     def __init__(self) -> None:
         self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
-
+        self.direction = Vector2(1,0)
     def draw_snake(self):
         for block in self.body:
             x_pos = int(block.x * cell_size)
@@ -28,6 +31,21 @@ class FRUIT:
         pygame.draw.rect(screen,(126,166,114),fruit_rect)
         #draw rectangle 
 
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+
+    def update(self):
+        self.snake.move_snake()
+
+    def update(self):
+        self.snake.move_snake()
+
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+
 pygame.init()
 cell_size = 40
 cell_number = 20
@@ -40,20 +58,28 @@ snake = SNAKE()
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE,150)
 
+main_game = MAIN()
+
 while True:
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
             pygame.quit
             sys.exit()
        if event.type == SCREEN_UPDATE:
-            snake.move_snake()
+           main_game.update()
        if event.type == pygame.KEYDOWN:
            if event.key == pygame.K_UP:
-               snake.direction
+               snake.direction = Vector2(0,-1)
+           if event.key == pygame.K_RIGHT:
+               snake.direction = Vector2(1,0)
+           if event.key == pygame.K_DOWN:
+               snake.direction = Vector2(0,1)
+           if event.key == pygame.K_LEFT:
+               snake.direction = Vector2(-1,0)
+               
            
            
     screen.fill((175,215,70))
-    fruit.draw_fruit()
-    snake.draw_snake()
+    main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)
