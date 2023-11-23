bbd987eb3f28b0e00c4d466c39bb26d185220d70
Frankataccess <frank.mccann.355@accesscreative.ac.uk>
2023-11-23 08:58:18 +0000

    snake

--- python/mini games/snake/snake.py
+++ python/mini games/snake/snake.py
@@ -75,7 +75,7 @@ class MAIN:
         
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
-                self.game_over
+                self.game_over()
 
     def game_over(self):
             pygame.quit()
@@ -104,13 +104,17 @@ while True:
             main_game.update() # Call the update method of the main game, not snake.move_snake()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                main_game.snake.direction = Vector2(0, -1)
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_RIGHT:
-                main_game.snake.direction = Vector2(1, 0)
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1, 0)
             if event.key == pygame.K_DOWN:
-                main_game.snake.direction = Vector2(0, 1)
+                if main_game.snake.direction.y != 1:
+                    main_game.snake.direction = Vector2(0, 1)
             if event.key == pygame.K_LEFT:
-                main_game.snake.direction = Vector2(-1, 0)
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1, 0)
 
     screen.fill((175, 215, 70))
     main_game.draw_elements()
